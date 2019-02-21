c99fa944b27c5af09fa92852611d972ab9a6c8ad
nictengkk <nictengkk@gmail.com>
2019-02-21 11:12:43 +0800

    Part 2 add buttons with incremental event handler

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -6,12 +6,20 @@ export class Counter extends Component {
     value: 0
   };
 
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
   render() {
     return (
       <div>
-        <span className="badge badge-secondary md">{this.state.value}</span>
-        <button>Increment</button>
-        <button>Decrement</button>
+        <span className="badge badge-secondary md mr-3 ml-3">
+          {this.state.value}
+        </span>
+        <button className="btn btn-primary" onClick={this.handleIncrement}>
+          +
+        </button>
+        <button className="btn btn-danger"> - </button>
       </div>
     );
   }
