9aa422566542957ada4e926cd417e7034283ae8b
nictengkk <nictengkk@gmail.com>
2019-02-21 11:20:22 +0800

    Part 3 change styles when increment or decrement is pressed

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -10,19 +10,29 @@ export class Counter extends Component {
     this.setState({ value: this.state.value + 1 });
   };
 
+  handleDecrement = () => {
+    this.setState({ value: this.state.value - 1 });
+  };
+
   render() {
     return (
       <div>
-        <span className="badge badge-secondary md mr-3 ml-3">
-          {this.state.value}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.state.value}</span>
         <button className="btn btn-primary" onClick={this.handleIncrement}>
           +
         </button>
-        <button className="btn btn-danger"> - </button>
+        <button className="btn btn-danger" onClick={this.handleDecrement}>
+          -
+        </button>
       </div>
     );
   }
+
+  getBadgeClasses = () => {
+    return this.state.value === 0
+      ? "badge badge-warning md mr-3 ml-3"
+      : "badge badge-primary md mr-3 ml-3";
+  };
 }
 
 export default Counter;
